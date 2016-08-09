60c89dd959d0912cc33a4defc3d15a86a09b7a2e
Nakamura Ayaka <kyarmkn@gmail.com>
2016-08-09 11:58:13 +0900

    phpプログラミング基礎学習1

---
+++ challenge1/challenge1_kiso.php
@@ -0,0 +1,56 @@
+<?php
+
+//1.
+ echo'Hello World.';
+
+ echo"<br><br>";
+ 
+//2.
+ echo'groove';
+ echo'-';
+ echo'gear';
+ 
+ echo"<br><br>";
+
+//3.
+ echo'中村彩夏です。';
+ echo"<br>";
+ echo'よろしくお願いします。';
+ 
+ echo'<br><br>';
+ 
+//4.と5.
+ const NUM=9;
+ $num=3;
+ 
+ echo NUM + $num;
+ echo'<br>';
+ echo NUM - $num;
+ echo'<br>';
+ echo NUM * $num;
+ echo'<br>';
+ echo NUM / $num;
+ echo'<br>';
+ echo'<br>';
+ 
+ 
+ //6.
+ $num == 1;
+ 
+ if($num == 1){
+  	  echo'1です！';
+ }
+ 
+ elseif($num == 2){
+ 	 echo'プログラミングキャンプ！'	;
+ }
+ 
+ elseif($num == a){
+ 	 echo'文字です！';
+ }
+ 
+ else{
+ 	 echo'その他です！';
+ }
+ 
+ ?>
