02dac0957293f9c4bd2eac400a20a080ced9b4ae
Nakamura Ayaka <kyarmkn@gmail.com>
2016-08-09 11:57:40 +0900

    phpプログラミング基礎学習2

---
+++ challenge2/challenge2_kiso9.php
@@ -0,0 +1,10 @@
+<?php
+//９．以下の順で、連想配列を作成してください。
+// 1に'AAA', 'hello'に'world', 'soeda'に33, 20に20
+$arr = array(
+	       1 => 'AAA',
+          'hello' => 'World',
+          'soeda' => 33,
+          20 => 20
+              );
+?>
